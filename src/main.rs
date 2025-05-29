6078aff54a97c3fee7830fcb501c79bc149d226c
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2025-05-30 02:43:31 +0900

    feat: :heavy_plus_sign: Add Bevy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use bevy::{
+    prelude::*,
+    log::LogPlugin,
+    asset::AssetMetaCheck,
+};
+
+const GAMETITLE: &str = "いっとくフロッガー";
+const WINDOW_SIZE: Vec2 = Vec2::new(640.0, 480.0);
+const BACKGROUND_COLOR: Color = Color::srgb(0.0, 0.0, 0.0);
+const LOG_FILTER: &str = "info,wgpu_core=warn,wgpu_hal=warn,ittoku_frogger=debug";
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    resolution: WINDOW_SIZE.into(),
+                    title: GAMETITLE.to_string(),
+                    ..Default::default()
+                }),
+                ..Default::default()
+            })
+            .set(LogPlugin {
+                filter: LOG_FILTER.into(),
+                level: bevy::log::Level::DEBUG,
+                ..Default::default()
+            })
+            .set(AssetPlugin {
+                meta_check: AssetMetaCheck::Never,
+                ..Default::default()
+            })
+        )
+        .insert_resource(ClearColor(BACKGROUND_COLOR))
+        .insert_resource(Time::<Fixed>::from_seconds(1.0 / 60.0))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    info_once!("setup");
+    debug!("spawn camera");
+    commands.spawn(Camera2d::default());
 }
