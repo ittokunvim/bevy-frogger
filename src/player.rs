441e9b2337cb3c5b7cd094ffdf60067a9cf11e7b
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2025-05-31 17:03:07 +0900

    feat: :hammer: Add player_change_animation system
    Issue: #2

--- src/player.rs
+++ src/player.rs
@@ -1,7 +1,7 @@
 use std::time::Duration;
 use bevy::prelude::*;
 
-use crate::PATH_IMAGE_PLAYER;
+use crate::{Direction, MoveEvent, PATH_IMAGE_PLAYER};
 
 const IMAGE_SIZE: UVec2 = UVec2::splat(18);
 const IMAGE_COLUMN: u32 = 8;
@@ -20,14 +20,20 @@ struct Player {
 
 impl Player {
     const FPS: u8 = 4;
+    const INDICES_LEFT: (usize, usize) = (8, 11);
+    const INDICES_RIGHT: (usize, usize) = (16, 19);
+    const INDICES_TOP: (usize, usize) = (24, 27);
+    const INDICES_BOTTOM: (usize, usize) = (0, 3);
 
     fn new() -> Self {
+        let first_sprite_index = Self::INDICES_BOTTOM.0;
+        let last_sprite_index = Self::INDICES_BOTTOM.1;
         let secs = 1.0 / Self::FPS as f32;
         let frame_timer = Timer::new(Duration::from_secs_f32(secs), TimerMode::Repeating);
 
         Self {
-            first_sprite_index: 0,
-            last_sprite_index: 3,
+            first_sprite_index,
+            last_sprite_index,
             frame_timer,
         }
     }
@@ -59,7 +65,7 @@ fn player_setup(
 }
 
 /// プレイヤーをアニメーションするための関数
-fn player_animate(
+fn player_animation(
     mut query: Query<(&mut Player, &mut Sprite), With<Player>>,
     time: Res<Time>,
 ) {
@@ -80,13 +86,56 @@ fn player_animate(
     }
 }
 
+/// イベントを受け取り、プレイヤーのアニメーションの振る舞いを決める関数
+fn player_change_animation(
+    mut events: EventReader<MoveEvent>,
+    mut query: Query<(&mut Player, &mut Sprite), With<Player>>,
+) {
+    info_once!("player_movement");
+
+    // イベントを受け取ったら処理を実行
+    for event in events.read() {
+        // プレイヤーの値を取得。プレイヤーがなければ処理を抜ける
+        let Ok((mut player, mut sprite)) = query.get_single_mut() else {
+            return;
+        };
+
+        // アニメーションのインデックスを更新
+        match **event {
+            Direction::Left => {
+                player.first_sprite_index = Player::INDICES_LEFT.0;
+                player.last_sprite_index = Player::INDICES_LEFT.1;
+            }
+            Direction::Right => {
+                player.first_sprite_index = Player::INDICES_RIGHT.0;
+                player.last_sprite_index = Player::INDICES_RIGHT.1;
+            }
+            Direction::Top => {
+                player.first_sprite_index = Player::INDICES_TOP.0;
+                player.last_sprite_index = Player::INDICES_TOP.1;
+            }
+            Direction::Bottom => {
+                player.first_sprite_index = Player::INDICES_BOTTOM.0;
+                player.last_sprite_index = Player::INDICES_BOTTOM.1;
+            }
+        }
+
+        // スプライトのインデックスを更新
+        if let Some(atlas) = &mut sprite.texture_atlas {
+            atlas.index = player.first_sprite_index;
+        };
+    }
+}
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app
             .add_systems(Startup, player_setup)
-            .add_systems(Update, player_animate)
+            .add_systems(Update, (
+                player_animation,
+                player_change_animation,
+            ))
         ;
     }
 }
