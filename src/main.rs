c82f2ba87421ef3ac76ceb074248b998fba8ad35
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2025-05-31 17:00:21 +0900

    feat: :heavy_plus_sign: Add key.rs
    Issue: #2

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use bevy::{
     asset::AssetMetaCheck,
 };
 
+mod key;
 mod player;
 
 const GAMETITLE: &str = "いっとくフロッガー";
@@ -11,6 +12,18 @@ const WINDOW_SIZE: Vec2 = Vec2::new(640.0, 480.0);
 const BACKGROUND_COLOR: Color = Color::srgb(0.0, 0.0, 0.0);
 const LOG_FILTER: &str = "info,wgpu_core=warn,wgpu_hal=warn,ittoku_frogger=debug";
 const PATH_IMAGE_PLAYER: &str = "ittoku-frogger/player.png";
+
+
+#[derive(Event, Deref, DerefMut)]
+struct MoveEvent(Direction);
+
+#[derive(Debug)]
+enum Direction {
+    Left,
+    Right,
+    Top,
+    Bottom
+}
 
 fn main() {
     App::new()
@@ -33,10 +46,12 @@ fn main() {
                 ..Default::default()
             })
         )
+        .add_event::<MoveEvent>()
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .insert_resource(Time::<Fixed>::from_seconds(1.0 / 60.0))
-        .add_plugins(player::PlayerPlugin)
         .add_systems(Startup, setup)
+        .add_plugins(key::KeyPlugin)
+        .add_plugins(player::PlayerPlugin)
         .run();
 }
 
