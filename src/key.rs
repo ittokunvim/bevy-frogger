63cc43c3eb40d76d7e46e5b51ef08853a1d9f15c
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2026-06-09 04:44:07 +0900

    Fixed codes
    Issue: #6

--- src/key.rs
+++ src/key.rs
@@ -7,47 +7,35 @@ const KEY_PLAYER_RIGHT: KeyCode = KeyCode::ArrowRight;
 const KEY_PLAYER_TOP: KeyCode = KeyCode::ArrowUp;
 const KEY_PLAYER_BOTTOM: KeyCode = KeyCode::ArrowDown;
 
-fn key_player_move_left(
-    mut events: EventWriter<MoveEvent>,
-    keyboard_input: Res<ButtonInput<KeyCode>>,
-) {
+fn key_player_move_left(mut commands: Commands, keyboard_input: Res<ButtonInput<KeyCode>>) {
     info_once!("key_player_move_left");
 
     if keyboard_input.just_pressed(KEY_PLAYER_LEFT) {
-        events.send(MoveEvent(Direction::Left));
+        commands.trigger(MoveEvent(Direction::Left));
     }
 }
 
-fn key_player_move_right(
-    mut events: EventWriter<MoveEvent>,
-    keyboard_input: Res<ButtonInput<KeyCode>>,
-) {
+fn key_player_move_right(mut commands: Commands, keyboard_input: Res<ButtonInput<KeyCode>>) {
     info_once!("key_player_move_right");
 
     if keyboard_input.just_pressed(KEY_PLAYER_RIGHT) {
-        events.send(MoveEvent(Direction::Right));
+        commands.trigger(MoveEvent(Direction::Right));
     }
 }
 
-fn key_player_move_top(
-    mut events: EventWriter<MoveEvent>,
-    keyboard_input: Res<ButtonInput<KeyCode>>,
-) {
+fn key_player_move_top(mut commands: Commands, keyboard_input: Res<ButtonInput<KeyCode>>) {
     info_once!("key_player_move_top");
 
     if keyboard_input.just_pressed(KEY_PLAYER_TOP) {
-        events.send(MoveEvent(Direction::Top));
+        commands.trigger(MoveEvent(Direction::Top));
     }
 }
 
-fn key_player_move_bottom(
-    mut events: EventWriter<MoveEvent>,
-    keyboard_input: Res<ButtonInput<KeyCode>>,
-) {
+fn key_player_move_bottom(mut commands: Commands, keyboard_input: Res<ButtonInput<KeyCode>>) {
     info_once!("key_player_move_bottom");
 
     if keyboard_input.just_pressed(KEY_PLAYER_BOTTOM) {
-        events.send(MoveEvent(Direction::Bottom));
+        commands.trigger(MoveEvent(Direction::Bottom));
     }
 }
 
@@ -55,13 +43,14 @@ pub struct KeyPlugin;
 
 impl Plugin for KeyPlugin {
     fn build(&self, app: &mut App) {
-        app
-        .add_systems(Update, (
+        app.add_systems(
+            Update,
+            (
                 key_player_move_left,
                 key_player_move_right,
                 key_player_move_top,
                 key_player_move_bottom,
-            ))
-        ;
+            ),
+        );
     }
 }
